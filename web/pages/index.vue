2869219b52943783cff5d2e6d75c4c7ec682902c
Douglas Cerqueira <douglasdacruzz@gmail.com>
2023-04-13 19:41:39 -0300

    :tada:

---
+++ web/pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <button @click="goFirstPage">First page</button>
+  <button @click="goSecondPage">Second page</button>
+</template>
+<script setup>
+function goFirstPage() {
+  navigateTo("/first-page");
+}
+function goSecondPage() {
+  navigateTo("/second-page");
+}
+</script>
